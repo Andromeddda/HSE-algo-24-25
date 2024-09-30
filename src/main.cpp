9ff4f395cd7809927085f8cd8747b2a568f4d53e
Andromeddda <andrey.shik2005@gmail.com>
2024-09-30 18:23:46 +0300

    [AUTOMATIC] Synchronize remote and local

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-// #include <cstdio>
+#include <cstdio>
 #include <cstdlib>
 // #include <cstring>
 // #include <assert.h>
@@ -24,88 +24,29 @@
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
-typedef typename std::uniform_real_distribution<double> Dist;
-typedef typename std::default_random_engine Engine;
-
 using namespace std;
 
-struct Point {
-    double x;
-    double y;
-
-    Point(double X, double Y) : x(X), y(Y) {}
-};
-
-struct Circle {
-    Point c;
-    double r;
-
-    Circle(double X, double Y, double R) : c(X, Y), r(R) {}
-};
-
-double distance(const Point& A, const Point& B);
-bool coverage(const Point& A, const vector<Circle>& circles);
-size_t Jinn(const vector<Circle>& circles, size_t precision);
-
-const size_t p = 100000;
+void play(int n);
 
 int 
 main(void) 
 {
-    size_t N;
-    cin >> N;
+    int m;
+    cin >> m;
 
-    auto circles = vector<Circle>();
-
-    for (auto i = 0LU; i < N; i++)
-    {
-        double x, y, r;
-        cin >> x >> y >> r;
-
-        circles.push_back(Circle(x, y, r));
+    for (int i = 0; i < m; i++) {
+        play(i + 2);
     }
-
-    size_t count = Jinn(circles, p);
-
-    cout << ((double)count / (double)p) * 100.0;
 
     return 0;
 }
 
-double 
-distance(const Point& A, const Point& B) 
-{   
-    double dx = A.x - B.x;
-    double dy = A.y - B.y;
-    return sqrt((dx * dx) + (dy * dy));
-}
-
-bool 
-coverage(const Point& A, const vector<Circle>& circles)
-{
-    for (Circle C : circles)
-    {
-        double R = distance(A, C.c);
-        if (C.r - R >= 0.0)
-            return true;
+void play(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = i + 1; j <= n; j++) {
+            printf("? %d %d\n", i, j);
+        }
     }
-    return false;
-}
-
-size_t
-Jinn (const vector<Circle>& circles, size_t precision) 
-{
-    size_t res = 0;
-
-    Dist dist;
-    Engine re;
-
-    for (; precision != 0; precision--) 
-    {
-        Point A = Point(dist(re), dist(re));
-        if (coverage(A, circles))
-            res++;
-    }
-    return res;
+    printf("! %d %d\n", n / 2, n / 2 + 1);
 }
 
